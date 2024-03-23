2d2571c6371fde0adb7494393b95d0f7b3122180
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-03-24 01:23:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,21 @@
 import streamlit as st
 import google.generativeai as genai
-
-# Configure GenerativeAI
 genai.configure(api_key="AIzaSyDKcxALky8LiROaxb0RGMw8TLLOcujMRMY")
-
-# Initialize GenerativeModel
 model = genai.GenerativeModel(model_name="gemini-pro")
-
-# Define function to generate recipe
-def generate_recipe(preference, ingredients):
-    prompt = f'"As an experienced culinarian and as a person who not only knows a variety of dishes but can also cook them, Given the following list of ingredients, suggest a suitable recipe to cook. The ingredients: {", ".join(ingredients)}. The user has a preference for {preference} dishes."'
+def generate_recipe(preference, ingredients,allergies):
+    prompt = f'"As an experienced culinarian and as a person who not only knows a variety of dishes but can also cook them, Given the following list of ingredients, suggest a suitable recipe to cook. The ingredients: {", ".join(ingredients)}. The user has a preference for {preference} dishes, and he user is alergic to these ingredients:{allergies}."'
     response = model.generate_content(prompt)
     return response.text
 
-# Streamlit app
 def main():
     st.title("Recipe Generator")
-    
-    # User input
-    preference = st.selectbox("Select your preference:", ["vegetarian", "non-vegetarian"])
+    preference = st.selectbox("Select your preference:", ["vegetarian", "non-vegetarian","vegan"])
     ingredients_input = st.text_input("Enter ingredients separated by comma (e.g., onion, tomato, potato):")
     ingredients = [ingredient.strip() for ingredient in ingredients_input.split(",")]
+    allergies=st.text_input("Do you have any allergies?(If None enter None):")
     
-    # Generate recipe
     if st.button("Generate Recipe"):
-        recipe = generate_recipe(preference, ingredients)
+        recipe = generate_recipe(preference, ingredients,allergies)
         st.subheader("Generated Recipe")
         st.markdown(recipe)
 
